5dfd0062ce5b075559e64791bf52e1247b164499
ferdouszihad <f.zihad333@gmail.com>
2024-01-25 17:30:41 +0600

    common function implemented and reduce code-repeat

---
+++ script.js/function.js
@@ -0,0 +1,21 @@
+function getElementById(id) {
+  const element = document.getElementById(id);
+  return element;
+}
+
+function getElementText(id) {
+  const element = document.getElementById(id);
+  const elementText = element.innerText;
+  return elementText;
+}
+function getElementNumber(id) {
+  const element = document.getElementById(id);
+  const elementText = element.innerText;
+  const elementNum = parseInt(elementText);
+  return elementNum;
+}
+
+function setElementText(id, newValue) {
+  const element = document.getElementById(id);
+  element.innerText = newValue;
+}
--- script.js/index.js
+++ script.js/index.js
@@ -8,8 +8,7 @@ function getRandomAphabet() {
   randomNum = parseInt(randomNum);
   const randomAlphabet = arrayOfAllAlphabets[randomNum]; // getting a randomAlphabet
   console.log(randomAlphabet);
-  const alphabetBox = document.getElementById("current-alphabet"); //getting the alphabet element from html
-  alphabetBox.innerHTML = randomAlphabet;
+  setElementText("current-alphabet", randomAlphabet);
 }
 getRandomAphabet();
 
@@ -20,22 +19,17 @@ document.addEventListener("keyup", function (e) {
   }
 
   const pressedButtonKey = e.key;
-  console.log(pressedButtonKey);
-  const alphabetBox = document.getElementById("current-alphabet"); //getting the alphabet element
-  const currentAlphabet = alphabetBox.innerText;
+  //   console.log(pressedButtonKey);
+  const currentAlphabet = getElementText("current-alphabet");
+
   if (currentAlphabet == pressedButtonKey) {
-    const scoreBox = document.getElementById("score");
-    const scoreCountText = scoreBox.innerText;
-    let scoreCountNum = parseInt(scoreCountText);
-    console.log(scoreCountNum);
-    scoreBox.innerText = scoreCountNum + 1;
+    let scoreCountNum = getElementNumber("score");
+    setElementText("score", scoreCountNum + 1);
+
     getRandomAphabet();
   } else {
-    const lifeBox = document.getElementById("life");
-    const lifeCountText = lifeBox.innerText;
-    let lifeCountNum = parseInt(lifeCountText);
-    console.log(lifeCountNum);
-    lifeBox.innerText = lifeCountNum - 1;
+    let lifeCountNum = getElementNumber("life");
+    setElementText("life", lifeCountNum - 1);
   }
 });
 
